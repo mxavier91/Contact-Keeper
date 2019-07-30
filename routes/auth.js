ea3d059b4cfe081ce07ac887ac52196cab90bc16
Malcolm Thomas <mxavier17@gmail.com>
2019-07-29 20:17:42 -0400

    Began creating the began. Set up the routes, and initalized the backend

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+// Two Routes. 1. Get the logged in User 2. Log in a User
+
+const express = require('express');
+const router = express.Router();
+
+// @route    GET api/auth
+// @desc     Get logged in user
+// @access   Private
+router.get('/', (req, res) => {
+  res.send('Get logged in User');
+});
+
+// @route    POST api/auth
+// @desc     Auth user & get token
+// @access   Public
+router.post('/', (req, res) => {
+  res.send('Log in User');
+});
+
+module.exports = router;
