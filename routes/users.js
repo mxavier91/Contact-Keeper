208794b5cae629e342b97bb441d49b53fc34341f
Malcolm Thomas <mxavier17@gmail.com>
2019-08-06 19:45:22 -0400

    Hashed the users password, and created a JSON web token with a registered user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 // Register User
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../Models/User');
@@ -20,13 +23,58 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        // 400 = Bad Request
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      // Creates a new instance of a user
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save user to the database
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      // 500 is a server error
+      res.status(500).send('Server Error');
+    }
   }
 );
 
