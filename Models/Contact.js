6e16b26c4c6718233ba589d4639773a6a37ef57c
Malcolm Thomas <mxavier17@gmail.com>
2019-08-07 18:54:54 -0400

    Created a Model for the contact resource. Also worked on the Get Route. We can now get all of the Contacts for a logged in User

---
+++ Models/Contact.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  phone: {
+    type: String
+  },
+  type: {
+    type: String,
+    default: 'personal'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// 'user' = User model
+module.exports = mongoose.model('contact', ContactSchema);
