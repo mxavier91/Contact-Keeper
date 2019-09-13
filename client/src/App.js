529e82f1bf9ea6bd62da450850ed5161a6403fba
Malcolm Thomas <mxavier17@gmail.com>
2019-09-12 21:40:16 -0400

    Continued working on the client side of the app. Added an animation for when a User creates or deletes an entry. I also began working on the Authentication. I create the context and I create a Login and RegisterComponent

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,32 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './Components/layout/NavBar';
 import Home from './Components/pages/Home';
 import About from './Components/pages/About';
+import Register from './Components/auth/Register';
+import Login from './Components/auth/Login';
 
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <NavBar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <NavBar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
